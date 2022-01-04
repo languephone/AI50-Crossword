941512fdd3d1054c6333cfe4ae28aafcef83804a
languephone <73309519+languephone@users.noreply.github.com>
2022-01-04 22:03:47 +0000

    Provisional assignment_complete method.

--- generate.py
+++ generate.py
@@ -173,7 +173,13 @@ class CrosswordCreator():
         Return True if `assignment` is complete (i.e., assigns a value to each
         crossword variable); return False otherwise.
         """
-        raise NotImplementedError
+
+        for variable in self.crossword.variables:
+            if not len(assignment.get(variable)) > 0:
+                return False
+
+        return True
+
 
     def consistent(self, assignment):
         """
